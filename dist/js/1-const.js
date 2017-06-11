e495cf7fbfeda52dbce7e4a51ff5cb4c8b7f920d
abaozhi <904740350@qq.com>
2017-06-11 15:25:22 +0800

    const
    第一章的const

---
+++ dist/js/1-const.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * Created by huangjian on 2017/6/2.
+ */
+//声明复合类型的常量
+var obj = {}; //对象
+obj.name = 'bz';
+console.log(obj);
+
+var array = [];
+array.push('i');
+console.log(array);
+console.log(array.length);
+//array = ['abaozhi'] 报错
+//冻结对象obj.freeze()...
+
+var a = 10; //全局
+console.log(window.a);
+var b = 11; //局部
+console.log(window.b);
+//# sourceMappingURL=1-const.js.map
